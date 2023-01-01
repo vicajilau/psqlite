6eb5067e665fc81fda5c684140e61a5a170e4e10
Victor Carreras <vicajilau@gmail.com>
2023-01-01 10:25:45 +0100

    Documentation improved

--- lib/src/psqlite.dart
+++ lib/src/psqlite.dart
@@ -22,17 +22,13 @@ class PSQLite {
   /// Used to create unit tests. Defaults to false.
   bool _isMocked;
 
-  /// Build the necessary database using lazy programming.
-  Future<Database> _getDatabase() async {
-    _database ??= (_isMocked)
-        ? await _mockedDataBase()
-        : _database = await _initializeDB();
-    return _database!;
-  }
-
+  /// Return the table related in the current database.
   TableDb getTable() => _table;
+
+  /// Set a new table in the current database.
   void setTable(TableDb table) => _table = table;
 
+  /// Return the version of the database.
   int getVersion() => _version;
 
   /// Set the version of the database.
@@ -60,30 +56,15 @@ class PSQLite {
         _isMocked = isMocked,
         _dbName = '${table.getName()}.db';
 
-  /// Create a mocked database.
-  Future<Database> _mockedDataBase() async {
-    return await openDatabase(inMemoryDatabasePath, version: 1,
-        onCreate: (db, version) async {
-      await db.execute(_table.getCreateDbRequest());
-    });
-  }
-
-  /// Create a real database.
-  Future<Database> _initializeDB() async {
-    // Open the database and store the reference.
-    return await openDatabase(
-      join(await getDatabasesPath(), _dbName),
-      onCreate: (db, version) {
-        // Run the CREATE TABLE statement on the database.
-        return db.execute(
-          _table.getCreateDbRequest(),
-        );
-      },
-      version: _version,
-    );
-  }
-
-  /// Define a function that inserts a Object into the database
+  /// Define a function that inserts a Object into the table.
+  ///
+  /// [object] is the ObjectStored object to insert. Optionally,
+  /// [conflictAlgorithm] is the algorithm in case of conflict,
+  /// by default the oldest is replaced by the newest.
+  ///
+  /// ```
+  /// await db.insertElement(user);
+  /// ```
   Future<void> insertElement(ObjectStored object,
       [ConflictAlgorithm conflictAlgorithm = ConflictAlgorithm.replace]) async {
     final db = await _getDatabase();
@@ -94,7 +75,14 @@ class PSQLite {
     );
   }
 
-  /// A method that retrieves all the objects from the current table.
+  /// A method that returns a table element based on its primary key.
+  ///
+  /// [primaryKey] is the primary key value to search for.
+  ///
+  /// Returns the element associated with that primary key, or null if not found:
+  /// ```
+  ///  final user = await db.getElementBy("4");
+  /// ```
   Future<Map<String, dynamic>?> getElementBy(String primaryKey) async {
     // Query the table for all The Elements.
     final db = await _getDatabase();
@@ -111,46 +99,20 @@ class PSQLite {
     }
   }
 
-  List<dynamic>? _getWhereFilters(List<FilterDb>? filters) {
-    if (filters == null || filters.isEmpty) {
-      return null;
-    }
-    List<dynamic> result = [];
-
-    for (int i = 0; i < filters.length; i++) {
-      final value = filters[i].getValue();
-      result.add(value);
-    }
-    return result;
-  }
-
-  String? _getWhereSentence(List<FilterDb>? filters) {
-    String sentence = '';
-    if (filters == null || filters.isEmpty) {
-      return null;
-    }
-    for (int i = 0; i < filters.length; i++) {
-      if (!_table.existColumnWith(filters[i].getField())) {
-        throw Exception('The ${filters[i].getField()} does not exist.');
-      }
-      sentence += filters[i].getSqlWhere();
-      if (i != filters.length - 1) {
-        sentence += ' AND ';
-      }
-    }
-    return sentence;
-  }
-
   /// Retrieves all elements from the table that match the list of filters.
   /// An empty filter listing will return all the objects from the current table.
   ///
   /// [where] is the optional WHERE clause to apply when updating. Passing empty
   /// will delete all rows.
   ///
-  /// Returns all items matching the filters, or otherwise, all items in the table.
+  /// Returns all items matching the filters:
   /// ```
-  ///  final allElements = await db.getElements();
-  ///  final minorElements = await db.getElements(where: [FilterDb('age', 18, ConditionDb.less)]);
+  /// final minorElements = await db.getElements(where: [FilterDb('age', 18, ConditionDb.less)]);
+  /// ```
+  ///
+  /// Returns all items in the table:
+  /// ```
+  /// final allElements = await db.getElements();
   /// ```
   Future<List<Map<String, dynamic>>> getElements(
       { List<FilterDb> where = const []}) async {
@@ -159,7 +121,7 @@ class PSQLite {
     final response = await db.query(_table.getName(),
         where: _getWhereSentence(where),
         // Prevent SQL injection.
-        whereArgs: _getWhereFilters(where));
+        whereArgs: _getWhereArgs(where));
     if (response.isEmpty) {
       return [];
     } else {
@@ -168,6 +130,12 @@ class PSQLite {
   }
 
   /// Update the given object in the current table.
+  ///
+  /// [object] is the ObjectStored object to update.
+  ///
+  /// ```
+  /// await db.updateElement(user);
+  /// ```
   Future<void> updateElement(ObjectStored object) async {
     final db = await _getDatabase();
     final filter = FilterDb(_table.getPrimaryColumn().getName(),
@@ -182,6 +150,13 @@ class PSQLite {
   }
 
   /// Remove the given object in the current table.
+  ///
+  /// [object] is the ObjectStored object to delete.
+  ///
+  /// Returns the element was deleted or not.
+  /// ```
+  /// bool result = await db.deleteElement(user);
+  /// ```
   Future<bool> deleteElement(ObjectStored object) async {
     final db = await _getDatabase();
     final filter = FilterDb(_table.getPrimaryColumn().getName(),
@@ -203,7 +178,7 @@ class PSQLite {
   ///
   /// Returns the number of rows affected.
   /// ```
-  ///  int count = await db.deleteElements(where: [FilterDb('age', 18, ConditionDb.less)]);
+  /// int count = await db.deleteElements(where: [FilterDb('age', 18, ConditionDb.less)]);
   /// ```
   Future<int> deleteElements({List<FilterDb> where = const []}) async {
     // Query the table for all The Elements.
@@ -211,7 +186,7 @@ class PSQLite {
     final numberOfElements = await db.delete(_table.getName(),
         where: _getWhereSentence(where),
         // Prevent SQL injection.
-        whereArgs: _getWhereFilters(where));
+        whereArgs: _getWhereArgs(where));
     return numberOfElements;
   }
 
@@ -219,5 +194,73 @@ class PSQLite {
   Future<void> clearTable() async {
     final db = await _getDatabase();
     await db.delete(_table.getName());
+  }
+}
+
+/// Private PSQLite operations.
+extension PrivatePSQLite on PSQLite {
+  /// Build the necessary database using lazy programming.
+  Future<Database> _getDatabase() async {
+    _database ??= (_isMocked)
+        ? await _mockedDataBase()
+        : _database = await _initializeDB();
+    return _database!;
+  }
+
+  /// Function that gets internally the values or arguments of the where statement
+  /// If the list of filters is empty, it will return null instead.
+  List<dynamic>? _getWhereArgs(List<FilterDb>? filters) {
+    if (filters == null || filters.isEmpty) {
+      return null;
+    }
+    List<dynamic> result = [];
+
+    for (int i = 0; i < filters.length; i++) {
+      final value = filters[i].getValue();
+      result.add(value);
+    }
+    return result;
+  }
+
+  /// Function that gets internally the WHERE part of the SQL statement from the list of filters.
+  /// If the list of filters is empty, it will return null instead.
+  String? _getWhereSentence(List<FilterDb>? filters) {
+    String sentence = '';
+    if (filters == null || filters.isEmpty) {
+      return null;
+    }
+    for (int i = 0; i < filters.length; i++) {
+      if (!_table.existColumnWith(filters[i].getField())) {
+        throw Exception('The ${filters[i].getField()} does not exist.');
+      }
+      sentence += filters[i].getSqlWhere();
+      if (i != filters.length - 1) {
+        sentence += ' AND ';
+      }
+    }
+    return sentence;
+  }
+
+  /// Create a mocked database.
+  Future<Database> _mockedDataBase() async {
+    return await openDatabase(inMemoryDatabasePath, version: 1,
+        onCreate: (db, version) async {
+          await db.execute(_table.getCreateDbRequest());
+        });
+  }
+
+  /// Create a real database.
+  Future<Database> _initializeDB() async {
+    // Open the database and store the reference.
+    return await openDatabase(
+      join(await getDatabasesPath(), _dbName),
+      onCreate: (db, version) {
+        // Run the CREATE TABLE statement on the database.
+        return db.execute(
+          _table.getCreateDbRequest(),
+        );
+      },
+      version: _version,
+    );
   }
 }
